58769058d38de40417be0bd8a3eb7a8e22ed0b92
KristijanArmeni <kristijan.armeni@gmail.com>
2019-03-27 15:40:32 +0100

    added function for averaging membrane voltages

--- snn_stats.py
+++ snn_stats.py
@@ -13,3 +13,24 @@ def avg_frate(recording):
     frate = total_spikes/total_time
 
     return frate
+
+
+def avg_states(data, toi=None):
+
+    """
+    Compute average membrane voltage per neuron per trial
+
+    :param data: array of membrane voltage (trial x neurons x time) created in net.config_recording().
+    :param toi: list of integers, onset and offset times
+    :return out: ndarray (trials x neurons), average voltage
+    """
+
+    # get timing sample points
+    ton = (np.abs(data-toi[0])).argmin()
+    toff = (np.abs(data-toi[1])).argmin()
+
+    # take average over selected time period
+    out = np.nanmean(a=data[:, :, ton:toff], axis=2)
+
+    return out
+
