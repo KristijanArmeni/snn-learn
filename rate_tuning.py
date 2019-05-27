6f021def40577cf1e1e088800fce02db3f05181e
KristijanArmeni <kristijan.armeni@gmail.com>
2019-05-27 18:33:07 +0200

    included optional use of 'rate_tuning2' method

--- rate_tuning.py
+++ rate_tuning.py
@@ -2,7 +2,7 @@
 
 USAGE EXAMPLE:
 
-python rate_tuning.py <[100, 500, 1200]> <300> <path_for_saving>
+python rate_tuning.py <[100, 500, 1200]> <300> <code_version> <path_for_saving>
 
 SYNTAX
 
@@ -20,6 +20,7 @@ import sys
 
 sizes = list(map(int, sys.argv[1].strip('[]').split(',')))
 n_trl = int(sys.argv[2])
+version = sys.argv[3]
 
 # ===== PARAMETERS ===== #
 
@@ -34,7 +35,7 @@ dat = load("/project/3011085.04/snn/data/raw/stimuli_1-4.pkl")
 # subset the dataset
 tmp = Dataset(sequence=dat[0:n_trl][0], response=dat[0:n_trl][1], encoding=dat[0:n_trl][2])
 
-tunings = []
+# seeds for N = [100, 500]: in: 55, rec:155
 
 for N in sizes:
 
@@ -50,12 +51,18 @@ for N in sizes:
     net.config_recording(n_neurons=net.neurons["N"], t=parameters.sim["t"], dataset=tmp, downsample=False)
     step = parameters.step_current(t=net.recording["t_orig"], on=0, off=0.05, amp=4.4e-9)
 
-    sel = net.rate_tuning(parameters=parameters, input_current=step, dataset=tmp,
-                          input_scale=2.3, input_step=0.1,
-                          rec_scale=1e-9, rec_step=0.2e-9,
-                          targets=[1.8, 2.2, 4.5, 5.5],
-                          N_max=50,
-                          skip_input=False)
-    tunings.append(sel)
+    if version == "rate_tuning":
+        sel = net.rate_tuning(parameters=parameters, input_current=step, dataset=tmp,
+                              input_scale=2.3, input_step=0.1,
+                              rec_scale=1e-9, rec_step=0.2e-9,
+                              targets=[1.8, 2.2, 4.5, 5.5],
+                              N_max=50, skip_input=False)
 
-save(tunings, sys.argv[3])
+    elif version == "rate_tuning2":
+
+        sel = net.rate_tuning2(parameters=parameters, input_current=step, dataset=tmp,
+                               init_scales=[2.3, 1e-9],
+                               targets=[2, 5], margins=[0.2, 0.5],
+                               N_max=50, skip_input=False)
+
+    save(sel, sys.argv[4] + "/" + "tuning-{}".format(N))
