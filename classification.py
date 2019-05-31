0fac18f7a50fd06c4f72b14edd574e58e11cc67e
KristijanArmeni <kristijan.armeni@gmail.com>
2019-05-31 12:15:38 +0200

    commit prior to converting it to standalone script

--- classification.py
+++ classification.py
@@ -1,154 +1,177 @@
 # ===== CLASSIFICATION ANALYSIS ===== #
 
-from sklearn.naive_bayes import GaussianNB
-from sklearn.model_selection import learning_curve, StratifiedKFold
-from sklearn.svm import SVC
+import numpy as np
+from matplotlib import pyplot as plt
+plt.style.use('seaborn-notebook')
 
-# load data
-net = load("/project/3011085.04/snn/data/raw/recording.pkl")
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import learning_curve, validation_curve
+from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
 
+#Own modules
+from util import load, save
+from plots import plot_scores
 
 # load the actual data
-x = load("/project/3011085.04/snn/data/interim/average_states.pkl")
-y = load("/project/3011085.04/snn/data/interim/responses.pkl")
+x100 = load("/project/3011085.04/snn/data/interim/train_x-100.pkl")
+x500 = load("/project/3011085.04/snn/data/interim/train_x-500.pkl")
+x1000 = load("/project/3011085.04/snn/data/interim/train_x-1000.pkl")
+
+# load the test data
+test_x100 = load("/project/3011085.04/snn/data/interim/test_x-100.pkl")
+test_x500 = load("/project/3011085.04/snn/data/interim/test_x-500.pkl")
+test_x1000 = load("/project/3011085.04/snn/data/interim/test_x-1000.pkl")
+
+y = load("/project/3011085.04/snn/data/interim/train_ds.pkl")
+y_test = load("/project/3011085.04/snn/data/interim/test_ds.pkl")
+
+# observed responses and stimuli
+y_rsp = y.response  # behavioral responses
+y_sym = y.sequence  # stimulus identity
+y_test_rsp = y_test.response
+y_test_sym = y_test.sequence
+
+# responses control
+y_rsp0_1 = np.random.RandomState(55).permutation(y_rsp)   # permuted responses
+y_rsp0_2 = np.roll(y_rsp, int((len(y_rsp)/2)))            # shifted resposnes
+
+y_test_rsp0_1 = np.random.RandomState(55).permutation(y_test_rsp)   # permuted responses
+y_test_rsp0_2 = np.roll(y_test_rsp, int((len(y_test_rsp)/2)))            # shifted resposnes
+
+# stimulus identity control
+y_sym0_1 = np.random.RandomState(55).permutation(y_sym)   # permuted responses
+y_sym0_2 = np.roll(y_sym, int((len(y_sym)/2)))            # shifted resposnes
+
+y_test_sym0_1 = np.random.RandomState(55).permutation(y_test_sym)   # permuted responses
+y_test_sym0_2 = np.roll(y_test_sym, int((len(y_test_sym)/2)))        # shifted resposnes
+
+save(y_rsp0_1, "/project/3011085.04/snn/data/interim/train_y_rsp_perm.pkl")
+save(y_rsp0_2, "/project/3011085.04/snn/data/interim/train_y_rsp_shift.pkl")
+save(y_sym0_1, "/project/3011085.04/snn/data/interim/train_y_sym_perm.pkl")
+save(y_sym0_2, "/project/3011085.04/snn/data/interim/train_y_sym_shift.pkl")
 
 
-# create fake data
-x2 = np.array(x)
-x2[y] += 0.03  # add systematic average increase of 0.03 mV
-save(x2, "/project/3011085.04/snn/data/interim/average_states_constructed.pkl")
+# ===== LOGISTIC REGRESSION: RESPONSES-TRAINING CURVE ===== #
 
-plt.figure()
-plt.hist(x.flatten())
-plt.figure()
-plt.hist(x2.flatten())
+scaler = StandardScaler()
 
-# random responses
-rand_y = np.random.RandomState(55).permutation(y)
-save(rand_y, "/project/3011085.04/snn/data/interim/responses_surrogate.pkl")
+logit = LogisticRegression(fit_intercept=True, class_weight="balanced", solver="newton-cg", penalty="l2")
 
+reg = np.array([1e-4, 1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3, 1e4])
 
-# plot distributions
-yplot = np.zeros(y.shape, dtype=int)
-yplot[np.where(y)] = 1
-plt.figure()
-plt.hist(yplot)
+scores = []
 
-randyplot = np.zeros(rand_y.shape, dtype=int)
-randyplot[np.where(rand_y)] = 1
-plt.figure()
-plt.hist(randyplot)
+for i, x in enumerate([test_x100, test_x500, test_x1000]):
 
-# Learning curve: Naive bayes (imbalanced classes)
+    print("Fit nr {}".format(i))
 
-N, train_lc, val_lc = learning_curve(estimator=GaussianNB(), X=x, y=y, cv=5, train_sizes=np.linspace(0.1, 1, 7))
-N, train_lc_surr, val_lc_surr = learning_curve(estimator=GaussianNB(), X=x, y=rand_y, cv=5, train_sizes=np.linspace(0.1, 1, 7))
-N, train_lc_fake, val_lc_fake = learning_curve(estimator=GaussianNB(), X=x2, y=y, cv=5, train_sizes=np.linspace(0.1, 1, 7))
+    N = x[0, :, :].shape[1]
 
-scoresNBim = [["observed response", train_lc, val_lc, N],
-          ["permuted response (null)", train_lc_surr, val_lc_surr, N],
-          ["constructed feature", train_lc_fake, val_lc_fake, N]]
+    x_norm = scaler.fit_transform(x[0, :, :])  # (time_slice, trial, neuron): first dimension stores time window
 
-save(scoresNBim, "/project/3011085.04/snn/data/results/scoresNB_imbalanced.pkl")
+    # fit logistic regression
+    train_sc_obs, val_sc_obs = validation_curve(estimator=logit, X=x_norm, y=y_rsp, cv=2, param_name="C",
+                                                param_range=reg, scoring="balanced_accuracy")
+    train_sc_rnd, val_sc_rnd = validation_curve(estimator=logit, X=x_norm, y=y_rsp0_1, cv=2, param_name="C",
+                                                param_range=reg, scoring="balanced_accuracy")
+    train_sc_shf, val_sc_shf = validation_curve(estimator=logit, X=x_norm, y=y_rsp0_2, cv=2, param_name="C", param_range=reg,
+                                                scoring="balanced_accuracy")
 
-plot_scores(scoresNBim, title="Learning curve: Naive Bayes (5-fold cv, imbalanced classes)")
+    scores.append([["observed", train_sc_obs, val_sc_obs, reg],
+                ["permuted", train_sc_rnd, val_sc_rnd, reg],
+                ["shifted", train_sc_shf, val_sc_shf, reg]])
 
-# Learning curve: SVC (imbalanced classes)
+    save(scores, "/project/3011085.04/snn/data/results/validation-curve_LR-{}.pkl".format(N))
 
-svc = SVC(kernel="linear", C=1e10)
+plot_scores(exp[0], title="Validation curve: Logistic regression (100 neurons)",
+            xlabel="regularization parameter (C)", ylabel="balanced accuracy")
 
-N, train_lc, val_lc = learning_curve(estimator=svc, X=x, y=y, cv=5, train_sizes=np.linspace(0.1, 1, 7))
-N, train_lc_surr, val_lc_surr = learning_curve(estimator=svc, X=x, y=rand_y, cv=5, train_sizes=np.linspace(0.1, 1, 7))
-N, train_lc_fake, val_lc_fake = learning_curve(estimator=svc, X=x2, y=y, cv=5, train_sizes=np.linspace(0.1, 1, 7))
+plot_scores(exp[1], title="Validation curve: Logistic regression (500 neurons)",
+            xlabel="regularization parameter (C)", ylabel="balanced accuracy")
 
-scoresNBim = [["observed response", train_lc, val_lc, N],
-          ["permuted response (null)", train_lc_surr, val_lc_surr, N],
-          ["constructed feature", train_lc_fake, val_lc_fake, N]]
-
-save(scoresNBim, "/project/3011085.04/snn/data/results/scoresSVC_imbalanced.pkl")
-
-plot_scores(scoresNBim, title="Learning curve: SVC (5-fold cv, imbalanced classes)")
-
-# Equal the number of training samples
-sel_false = np.random.RandomState(55).choice(np.where(y == False)[0], replace=False, size=np.sum(y))
-sel = np.sort(np.concatenate((sel_false, (np.where(y == True)[0]))))
-
-rand_y2 = np.random.RandomState(55).permutation(y[sel])
-
-x2 = np.array(x[sel, :])
-x2[y[sel]] += 0.03  # add systematic average increase of 0.03 mV to membrane states
-
-# Define the cross-validation scheme
-
-cv = StratifiedKFold(n_splits=5, shuffle=False)
-
-N, train_lc, test_lc = learning_curve(estimator=GaussianNB(), X=x[sel, :], y=y[sel], cv=cv, train_sizes=np.linspace(0.1, 1, 7))
-N, train_lc_surr, test_lc_surr = learning_curve(estimator=GaussianNB(), X=x[sel, :], y=rand_y2, cv=cv, train_sizes=np.linspace(0.1, 1, 7))
-N, train_lc_fake, test_lc_fake = learning_curve(estimator=GaussianNB(), X=x2, y=y[sel], cv=cv, train_sizes=np.linspace(0.1, 1, 7))
-
-scoresNBb = [["observed response", train_lc, test_lc, N],
-          ["permuted response (null)", train_lc_surr, test_lc_surr, N],
-          ["constructed feature", train_lc_fake, test_lc_fake, N]]
-
-# save output
-save(scoresNBb, "/project/3011085.04/snn/data/results/scoresNB_balanced.pkl")
-
-plot_scores(scoresNBb, title="Learning curves: Naive Bayes (5-fold cross-validation, balanced classes)")
-
-# ==== SVM ===== #
-
-svc = SVC(kernel="linear", C=1e10)
-
-N, train_lc, test_lc = learning_curve(estimator=svc, X=x[sel, :], y=y[sel], cv=cv, train_sizes=np.linspace(0.1, 1, 7))
-N, train_lc_surr, test_lc_surr = learning_curve(estimator=svc, X=x[sel, :], y=rand_y2, cv=cv, train_sizes=np.linspace(0.1, 1, 7))
-N, train_lc_fake, test_lc_fake = learning_curve(estimator=svc, X=x2, y=y[sel], cv=cv, train_sizes=np.linspace(0.1, 1, 7))
-
-scoresSVC = [["observed response", train_lc, test_lc, N],
-            ["permuted response (null)", train_lc_surr, test_lc_surr, N],
-            ["constructed feature", train_lc_fake, test_lc_fake, N]]
-
-# save output
-save(scoresSVC, "/project/3011085.04/snn/data/results/scoresSVC_balanced.pkl")
-
-plot_scores(scoresSVC, title="Learning curves: SVM (5-fold cross-validation, balanced classes)")
+plot_scores(exp[2], title="Validation curve: Logistic regression (1000 neurons)",
+            xlabel="regularization parameter (C)", ylabel="balanced accuracy")
 
 
-plt.figure()
-plt.imshow(net.recording["V"][0,:,:], aspect="auto")
-plt.xlabel("time (sec)")
-plt.title("Trial {}".format(0+1))
+scaler = StandardScaler()
 
-plt.figure()
-plt.imshow(net.recording["spikes"][1,:,:], aspect="auto")
-plt.xlabel("time (sec)")
-plt.title("Trial {}".format(1+1))
+logit = LogisticRegression(fit_intercept=True, class_weight="balanced", solver="newton-cg", penalty="l2")
 
-# ===== DEMO ===== #
+sizes = np.array([0.2, 0.4, 0.6, 0.8, 1])
 
-seq, rsp, out_seq = generate_sequence(n_inner=4, n_outer=5, seed=95)
-inp = encode_input(sequence=seq)
-ds = Dataset(sequence=seq, response=rsp, encoding=inp)
+exp = []
+
+for i, x in enumerate([test_x100, test_x500, test_x1000]):
+
+    print("Fit nr {}".format(i))
+
+    N = x[0, :, :].shape[1]
+
+    x_norm = scaler.fit_transform(x[0, :, :])  # (time_slice, trial, neuron): first dimension stores time window
+
+    # fit logistic regression
+    N, train_sc_obs, val_sc_obs = learning_curve(estimator=logit, X=x_norm, y=y_test_rsp, cv=5, train_sizes=sizes,
+                                              scoring="balanced_accuracy")
+    N, train_sc_rnd, val_sc_rnd = learning_curve(estimator=logit, X=x_norm, y=y_test_rsp0_1, cv=5, train_sizes=sizes,
+                                              scoring="balanced_accuracy")
+    N, train_sc_shf, val_sc_shf = learning_curve(estimator=logit, X=x_norm, y=y_test_rsp0_2, cv=5, train_sizes=sizes,
+                                              scoring="balanced_accuracy")
+
+    exp.append([["observed", train_sc_obs, val_sc_obs, N],
+                ["permuted", train_sc_rnd, val_sc_rnd, N],
+                ["shifted", train_sc_shf, val_sc_shf, N]])
+
+    save(exp, "/project/3011085.04/snn/data/results/learning-curve2_LR-{}.pkl".format(N))
+
+plot_scores(exp[0], title="Learning curve: Logistic regression (100 neurons)",
+            xlabel="test set size (N)", ylabel="balanced accuracy")
+
+plot_scores(exp[1], title="Learning curve: Logistic regression (500 neurons)",
+            xlabel="test set size (N)", ylabel="balanced accuracy")
+
+plot_scores(exp[2], title="Learning curve: Logistic regression (1000 neurons)",
+            xlabel="test set size (N)", ylabel="balanced accuracy")
 
 
-# Initialize parameters
-parameters = Params(tmin=0, tmax=0.5, dt=0.001,   # simulation parameters
-                    tau_gsra=0.4, tau_gref=0.002)  # synaptic adaptation
+# ===== LOGISTIC REGRESSION: STIMULUS IDENTITY ===== #
 
-# initialize network instance
-net_demo = SNN(params=parameters, n_neurons=10, input_dim=8, output_dim=2, syn_adapt=False)
-net_demo.config_input_weights(mean=0.4, density=0.5, seed=1)
-net_demo.config_recurrent_weights(density=0.5, ex=0.8, seed=2)
+scaler = StandardScaler()
 
-# scaling
-net_demo.w["input"] *= 2.1
-net_demo.w["recurrent"] *= 3.7e-9
+symbol_logit = LogisticRegression(fit_intercept=True, multi_class="multinomial", max_iter=300, class_weight="balanced",
+                                  solver="newton-cg", penalty="l2")
 
-net_demo.config_recording(n_neurons=net_demo.neurons["N"], t=parameters.sim["t"], dataset=ds, downsample=None)
+reg = np.array([1e-4, 1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3, 1e4])
 
-input_current = parameters.step_current(t=net_demo.recording["t_orig"], on=0, off=0.5, amp=4.8e-9)
+exp = []
 
-net_demo.w["recurrent"][:] = 0  # disconnect recurrent weights
-net_demo.train(dataset=ds, current=input_current, reset_states="sentence")
+for i, x in enumerate([x100, x500, x1000]):
 
-plot_trial(model=net_demo, trial=2, variable="V")
-plot_time(model=net_demo, trial=2, variable="V")
+    print("Fit nr {}".format(i))
+
+    N = x[0, :, :].shape[1]
+
+    x_norm = scaler.fit_transform(x[0, :, :])  # (time_slice, trial, neuron): first dimension stores time window
+
+    # fit logistic regression
+    train_sc_obs, val_sc_obs = validation_curve(estimator=symbol_logit, X=x_norm, y=y_sym, cv=5, param_name="C",
+                                                param_range=reg, scoring="balanced_accuracy")
+    train_sc_rnd, val_sc_rnd = validation_curve(estimator=symbol_logit, X=x_norm, y=y_sym0_1, cv=5, param_name="C",
+                                                param_range=reg, scoring="balanced_accuracy")
+    train_sc_shf, val_sc_shf = validation_curve(estimator=symbol_logit, X=x_norm, y=y_sym0_2, cv=5, param_name="C", param_range=reg,
+                                                scoring="balanced_accuracy")
+
+    exp.append([["observed", train_sc_obs, val_sc_obs, reg],
+                ["permuted", train_sc_rnd, val_sc_rnd, reg],
+                ["shifted", train_sc_shf, val_sc_shf, reg]])
+
+save(exp, "/project/3011085.04/snn/data/results/learning-curve_LR-symbol-100-500-1000.pkl")
+
+plot_scores(exp[0], title="Validation curve (LR): symbol classification (100 neurons)",
+            xlabel="regularization parameter (C)", ylabel="balanced accuracy")
+
+plot_scores(exp[1], title="Validation curve (LR): symbol classification (500 neurons)",
+            xlabel="regularization parameter (C)", ylabel="balanced accuracy")
+
+plot_scores(exp[2], title="Validation curve (LR): symbol classification (1000 neurons)",
+            xlabel="regularization parameter (C)", ylabel="balanced accuracy")
+
