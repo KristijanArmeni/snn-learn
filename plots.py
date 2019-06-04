7b3c86b28476a64f0b387cf77722133319df557f
KristijanArmeni <kristijan.armeni@gmail.com>
2019-06-04 09:03:34 +0200

    updated plot_scores() to work with dicts

--- plots.py
+++ plots.py
@@ -32,28 +32,29 @@ def plot_scores(scores, title=None, xlabel=None, ylabel=None):
     fig, ax = plt.subplots(1, len(scores))
     fig.suptitle(title, size=18)
 
-    for i, data in enumerate(scores):
+    for i, key in enumerate(scores):
 
-        X = data[3]
+        data = scores[key]
+        X = data[2]
 
         # compute mean accuracy scores
-        train_mean = np.mean(data[1], 1)
-        test_mean = np.mean(data[2], 1)
-        train_std = np.std(data[1], 1)
-        test_std = np.std(data[2], 1)
+        train_mean = np.mean(data[0], 1)
+        test_mean = np.mean(data[1], 1)
+        train_std = np.std(data[0], 1)
+        test_std = np.std(data[1], 1)
 
         ax[i].fill_between(X, train_mean - train_std, train_mean + train_std,
                            alpha=0.1)
         ax[i].fill_between(X, test_mean - test_std, test_mean + test_std,
                            alpha=0.1)
 
-        ax[i].semilogx(X, np.mean(data[1], 1), '--o', label='training score')
-        ax[i].semilogx(X, np.mean(data[2], 1), '--o', label='test score')
+        ax[i].semilogx(X, np.mean(data[0], 1), '--o', label='training score')
+        ax[i].semilogx(X, np.mean(data[1], 1), '--o', label='test score')
 
         ax[i].set_ylim(0, 1.05)
         ax[i].set_xlim(X[0], X[-1])
         ax[i].set_xlabel(xlabel)
         if i == 0:
             ax[i].set_ylabel(ylabel)
-        ax[i].set_title(data[0], size=14)
+        ax[i].set_title(key, size=14)
         ax[i].legend(loc='best')
