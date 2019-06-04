27d7dce31ff3b18aacf3ccce789859df59da249f
KristijanArmeni <kristijan.armeni@gmail.com>
2019-06-04 08:57:48 +0200

    modified classification.py to be used as a script with a string argument that controls what classification is performed and output saving.

--- classification.py
+++ classification.py
@@ -1,177 +1,228 @@
 # ===== CLASSIFICATION ANALYSIS ===== #
 
 import numpy as np
+import sys
 from matplotlib import pyplot as plt
 plt.style.use('seaborn-notebook')
 
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import learning_curve, validation_curve
+from sklearn.model_selection import learning_curve, validation_curve, cross_val_score
 from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
 
 #Own modules
-from util import load, save
-from plots import plot_scores
+from util import load, save, Paths
+
+# shortucts to paths
+p = Paths()
+
+# 12ax task
+stim = load(p.raw + "/12ax-12k.pkl")
 
 # load the actual data
-x100 = load("/project/3011085.04/snn/data/interim/train_x-100.pkl")
-x500 = load("/project/3011085.04/snn/data/interim/train_x-500.pkl")
-x1000 = load("/project/3011085.04/snn/data/interim/train_x-1000.pkl")
-
-# load the test data
-test_x100 = load("/project/3011085.04/snn/data/interim/test_x-100.pkl")
-test_x500 = load("/project/3011085.04/snn/data/interim/test_x-500.pkl")
-test_x1000 = load("/project/3011085.04/snn/data/interim/test_x-1000.pkl")
-
-y = load("/project/3011085.04/snn/data/interim/train_ds.pkl")
-y_test = load("/project/3011085.04/snn/data/interim/test_ds.pkl")
+x100 = np.load(p.interim + "/states-100.npy")
+x500 = np.load(p.interim + "/states-500.npy")
+x1000 = np.load(p.interim + "/states-1000.npy")
 
 # observed responses and stimuli
-y_rsp = y.response  # behavioral responses
-y_sym = y.sequence  # stimulus identity
-y_test_rsp = y_test.response
-y_test_sym = y_test.sequence
+y_rsp = stim.response  # behavioral responses
+y_sym = stim.sequence  # stimulus identity
 
 # responses control
-y_rsp0_1 = np.random.RandomState(55).permutation(y_rsp)   # permuted responses
-y_rsp0_2 = np.roll(y_rsp, int((len(y_rsp)/2)))            # shifted resposnes
-
-y_test_rsp0_1 = np.random.RandomState(55).permutation(y_test_rsp)   # permuted responses
-y_test_rsp0_2 = np.roll(y_test_rsp, int((len(y_test_rsp)/2)))            # shifted resposnes
-
-# stimulus identity control
-y_sym0_1 = np.random.RandomState(55).permutation(y_sym)   # permuted responses
-y_sym0_2 = np.roll(y_sym, int((len(y_sym)/2)))            # shifted resposnes
-
-y_test_sym0_1 = np.random.RandomState(55).permutation(y_test_sym)   # permuted responses
-y_test_sym0_2 = np.roll(y_test_sym, int((len(y_test_sym)/2)))        # shifted resposnes
-
-save(y_rsp0_1, "/project/3011085.04/snn/data/interim/train_y_rsp_perm.pkl")
-save(y_rsp0_2, "/project/3011085.04/snn/data/interim/train_y_rsp_shift.pkl")
-save(y_sym0_1, "/project/3011085.04/snn/data/interim/train_y_sym_perm.pkl")
-save(y_sym0_2, "/project/3011085.04/snn/data/interim/train_y_sym_shift.pkl")
+y_rsp0 = np.roll(y_rsp, int((len(y_rsp)/2)))  # shifted responses
+y_sym0 = np.roll(y_sym, int((len(y_sym)/2)))  # shifted resposnes
 
 
-# ===== LOGISTIC REGRESSION: RESPONSES-TRAINING CURVE ===== #
+if sys.argv[1] == "validation-curve-response":
 
-scaler = StandardScaler()
+    # ===== LOGISTIC REGRESSION: RESPONSES-TRAINING CURVE ===== #
+    scaler = StandardScaler()
+    logit = LogisticRegression(fit_intercept=True, class_weight="balanced", solver="newton-cg", penalty="l2")
+    range = np.array([1e-4, 1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3, 1e4])
 
-logit = LogisticRegression(fit_intercept=True, class_weight="balanced", solver="newton-cg", penalty="l2")
+    trlidx = np.arange(0, 2000)
 
-reg = np.array([1e-4, 1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3, 1e4])
+    for i, x in enumerate([x100, x500, x1000]):
 
-scores = []
+        conditions = ["observed", "permuted"]
+        responses = {conditions[0]: y_rsp, conditions[1]: y_rsp0}
+        scores = {conditions[0]: None, conditions[1]: None}
 
-for i, x in enumerate([test_x100, test_x500, test_x1000]):
+        N = x[0, trlidx, :].shape[1]
+        x_norm = scaler.fit_transform(x[0, trlidx, :])  # (time_slice, trial, neuron): first dimension stores time window
 
-    print("Fit nr {}".format(i))
+        for j, condition in enumerate(conditions):
 
-    N = x[0, :, :].shape[1]
+            print("Validation curve for {} condition, N = {} and {} samples".format(condition, N, len(trlidx)))
 
-    x_norm = scaler.fit_transform(x[0, :, :])  # (time_slice, trial, neuron): first dimension stores time window
+            y = responses[condition][trlidx]
 
-    # fit logistic regression
-    train_sc_obs, val_sc_obs = validation_curve(estimator=logit, X=x_norm, y=y_rsp, cv=2, param_name="C",
-                                                param_range=reg, scoring="balanced_accuracy")
-    train_sc_rnd, val_sc_rnd = validation_curve(estimator=logit, X=x_norm, y=y_rsp0_1, cv=2, param_name="C",
-                                                param_range=reg, scoring="balanced_accuracy")
-    train_sc_shf, val_sc_shf = validation_curve(estimator=logit, X=x_norm, y=y_rsp0_2, cv=2, param_name="C", param_range=reg,
-                                                scoring="balanced_accuracy")
+            # fit logistic regression
+            train_sc, val_sc = validation_curve(estimator=logit, X=x_norm, y=y, cv=5, param_name="C",
+                                                param_range=range, scoring="balanced_accuracy")
 
-    scores.append([["observed", train_sc_obs, val_sc_obs, reg],
-                ["permuted", train_sc_rnd, val_sc_rnd, reg],
-                ["shifted", train_sc_shf, val_sc_shf, reg]])
+            scores[condition] = [train_sc, val_sc, range]
 
-    save(scores, "/project/3011085.04/snn/data/results/validation-curve_LR-{}.pkl".format(N))
-
-plot_scores(exp[0], title="Validation curve: Logistic regression (100 neurons)",
-            xlabel="regularization parameter (C)", ylabel="balanced accuracy")
-
-plot_scores(exp[1], title="Validation curve: Logistic regression (500 neurons)",
-            xlabel="regularization parameter (C)", ylabel="balanced accuracy")
-
-plot_scores(exp[2], title="Validation curve: Logistic regression (1000 neurons)",
-            xlabel="regularization parameter (C)", ylabel="balanced accuracy")
+        save(scores, p.results + "/validation-curve_response-{}.pkl".format(N))
 
 
-scaler = StandardScaler()
+elif sys.argv[1] == "learning-curve-response":
 
-logit = LogisticRegression(fit_intercept=True, class_weight="balanced", solver="newton-cg", penalty="l2")
+    scaler = StandardScaler()
+    logit = LogisticRegression(fit_intercept=True, class_weight="balanced", C=1.0, solver="newton-cg", penalty="l2")
+    sizes = np.array([0.2, 0.4, 0.6, 0.8, 1])
 
-sizes = np.array([0.2, 0.4, 0.6, 0.8, 1])
+    trlidx = np.arange(0, 2000)
 
-exp = []
+    for i, x in enumerate([x100, x500, x1000]):
 
-for i, x in enumerate([test_x100, test_x500, test_x1000]):
+        print("Fit nr {}".format(i))
 
-    print("Fit nr {}".format(i))
+        N = x[0, trlidx, :].shape[1]
+        x_norm = scaler.fit_transform(x[0, trlidx, :])  # (time_slice, trial, neuron): first dimension stores time window
 
-    N = x[0, :, :].shape[1]
+        conditions = ["observed", "permuted"]
+        responses = {conditions[0]: y_rsp, conditions[1]: y_rsp0}
+        scores = {conditions[0]: None, conditions[1]: None}
 
-    x_norm = scaler.fit_transform(x[0, :, :])  # (time_slice, trial, neuron): first dimension stores time window
+        for j, condition in enumerate(conditions):
 
-    # fit logistic regression
-    N, train_sc_obs, val_sc_obs = learning_curve(estimator=logit, X=x_norm, y=y_test_rsp, cv=5, train_sizes=sizes,
-                                              scoring="balanced_accuracy")
-    N, train_sc_rnd, val_sc_rnd = learning_curve(estimator=logit, X=x_norm, y=y_test_rsp0_1, cv=5, train_sizes=sizes,
-                                              scoring="balanced_accuracy")
-    N, train_sc_shf, val_sc_shf = learning_curve(estimator=logit, X=x_norm, y=y_test_rsp0_2, cv=5, train_sizes=sizes,
-                                              scoring="balanced_accuracy")
+            print("Learning curve for {} condition, N = {} and {} samples".format(condition, N, len(trlidx)))
 
-    exp.append([["observed", train_sc_obs, val_sc_obs, N],
-                ["permuted", train_sc_rnd, val_sc_rnd, N],
-                ["shifted", train_sc_shf, val_sc_shf, N]])
+            y = responses[condition][trlidx]
 
-    save(exp, "/project/3011085.04/snn/data/results/learning-curve2_LR-{}.pkl".format(N))
+            # fit logistic regression
+            N_sizes, train_sc, val_sc = learning_curve(estimator=logit, X=x_norm, y=y, cv=5,
+                                                 train_sizes=sizes, scoring="balanced_accuracy")
 
-plot_scores(exp[0], title="Learning curve: Logistic regression (100 neurons)",
-            xlabel="test set size (N)", ylabel="balanced accuracy")
+            scores[condition] = [train_sc, val_sc, N_sizes]
 
-plot_scores(exp[1], title="Learning curve: Logistic regression (500 neurons)",
-            xlabel="test set size (N)", ylabel="balanced accuracy")
+        save(scores, p.results + "/learning-curve_response-{}.pkl".format(N))
 
-plot_scores(exp[2], title="Learning curve: Logistic regression (1000 neurons)",
-            xlabel="test set size (N)", ylabel="balanced accuracy")
+elif sys.argv[1] == "validation-curve-stim":
+
+    # ===== LOGISTIC REGRESSION: STIMULUS IDENTITY, VALIDATION CURVE ===== #
+
+    scaler = StandardScaler()
+    logit = LogisticRegression(fit_intercept=True, multi_class="multinomial", max_iter=300, class_weight="balanced",                                  solver="newton-cg", penalty="l2")
+    range = np.logspace(-4, 4, 9, base=10)
+
+    for i, x in enumerate([x100, x500, x1000]):
+
+        N = x[0, :, :].shape[1]
+        x_norm = scaler.fit_transform(x[0, :, :])  # (time_slice, trial, neuron): first dimension stores time window
+
+        conditions = ["observed", "permuted"]
+        responses = {conditions[0]: y_rsp, conditions[1]: y_rsp0}
+        scores = {conditions[0]: None, conditions[1]: None}
+
+        for j, condition in enumerate(conditions):
+
+            y = responses[condition]
+
+            print("Validation curve for {} condition and N = {}".format(condition, N))
+
+            # fit logistic regression
+            train_sc, val_sc = validation_curve(estimator=logit, X=x_norm, y=y, cv=5, param_name="C",
+                                                        param_range=range, scoring="balanced_accuracy")
+
+            scores[condition] = [train_sc, val_sc, range]
+
+        save(scores, p.results + "/validation-curve_symbol-{}.pkl".format(N))
+
+elif sys.argv[1] == "learning-curve-stim":
+
+    # ===== LOGISTIC REGRESSION: STIMULUS IDENTITY, VALIDATION CURVE ===== #
+
+    scaler = StandardScaler()
+    logit = LogisticRegression(fit_intercept=True, multi_class="multinomial", C=1.0, max_iter=300, class_weight="balanced",                                  solver="newton-cg", penalty="l2")
+    sizes = np.array([0.2, 0.4, 0.6, 0.8, 1])
+
+    for i, x in enumerate([x100, x500, x1000]):
+
+        N = x[0, :, :].shape[1]
+        x_norm = scaler.fit_transform(x[0, :, :])  # (time_slice, trial, neuron): first dimension stores time window
+
+        conditions = ["observed", "permuted"]
+        responses = {conditions[0]: y_rsp, conditions[1]: y_rsp0}
+        scores = {conditions[0]: None, conditions[1]: None}
+
+        for j, condition in enumerate(conditions):
+
+            y = responses[condition]
+
+            print("Learning curve for {} condition and N = {}".format(condition, N))
+
+            # fit logistic regression
+            train_sc, val_sc = learning_curve(estimator=logit, X=x_norm, y=y, cv=5,
+                                              sizes=sizes, scoring="balanced_accuracy")
+
+            scores[condition] = [train_sc, val_sc, range]
+
+        save(scores, p.results + "/learning-curve_symbol-{}.pkl".format(N))
+
+elif sys.argv[1] == "stimulus-buildup":
+
+    x = np.load(p.interim + "/states-1000_reset_tau.npy")[5, :, :, :]  # take all time_windows (0 == entire time window)
+    responses = {"observed": y_rsp, "permuted": y_rsp0}
+
+    for i, key_y in responses:
+
+        y_t = responses[key_y][2000::]
+        y_0 = responses[key_y][1::]  # take labels
+
+        scores = []
+
+        # loop over time windows
+        for j in range(x.shape[1]):
+
+            print("\n[{:d}] Fitting time window starting at {:d} msec ...".format(j, j*10))
+
+            scaler = StandardScaler()
+            logit = LogisticRegression(fit_intercept=True, multi_class="multinomial", C=1.0, max_iter=300,
+                                       class_weight="balanced", solver="newton-cg", penalty="l2")
+
+            x_norm = scaler.fit_transform(X=x[i, :, :])  # x.shape = (n_samples, n_neurons)
+
+            accuracy = cross_val_score(estimator=logit, X=x_norm, y=y_t, cv=5, scoring="balanced_accuracy")
+            scores.append(accuracy)
+
+        save(scores, p.interim + "/buildup-{}.pkl".format(key_y))
 
 
-# ===== LOGISTIC REGRESSION: STIMULUS IDENTITY ===== #
+elif sys.argv[1] == "adaptation-curve":
 
-scaler = StandardScaler()
+    # load data; shape = (tau, time_window, trial, neuron)
+    x_r = np.load(p.interim + "/states-1000_reset_tau.npy")
+    x_c = np.load(p.interim + "/states-1000_noreset_tau.npy")
 
-symbol_logit = LogisticRegression(fit_intercept=True, multi_class="multinomial", max_iter=300, class_weight="balanced",
-                                  solver="newton-cg", penalty="l2")
+    states = {"reset": x_r, "noreset": x_c}
+    responses = {"observed": y_rsp[2000::], "permuted": y_rsp0[2000::]}
 
-reg = np.array([1e-4, 1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3, 1e4])
+    scaler = StandardScaler()
+    logit = LogisticRegression(fit_intercept=True, C=1.0, max_iter=300, class_weight="balanced",
+                               solver="newton-cg", penalty="l2")
 
-exp = []
+    # loop over conditions
+    for i, key_x in enumerate(states):
 
-for i, x in enumerate([x100, x500, x1000]):
+        # choose the first time window
+        x = states[key_x][:, 0, :, :]  # x.shape = (tau, n_samples, n_neurons)
 
-    print("Fit nr {}".format(i))
+        # loop over y labels
+        for h, key_y in enumerate(responses):
 
-    N = x[0, :, :].shape[1]
+            # choose the samples corresponding to the test set
+            y = responses[key_y]  # shape = (n_samples,)
+            scores = []
 
-    x_norm = scaler.fit_transform(x[0, :, :])  # (time_slice, trial, neuron): first dimension stores time window
+            # loop over tau values
+            for j in range(x.shape[0]):
 
-    # fit logistic regression
-    train_sc_obs, val_sc_obs = validation_curve(estimator=symbol_logit, X=x_norm, y=y_sym, cv=5, param_name="C",
-                                                param_range=reg, scoring="balanced_accuracy")
-    train_sc_rnd, val_sc_rnd = validation_curve(estimator=symbol_logit, X=x_norm, y=y_sym0_1, cv=5, param_name="C",
-                                                param_range=reg, scoring="balanced_accuracy")
-    train_sc_shf, val_sc_shf = validation_curve(estimator=symbol_logit, X=x_norm, y=y_sym0_2, cv=5, param_name="C", param_range=reg,
-                                                scoring="balanced_accuracy")
+                print("\nRunning states with {}, {} labels and tau iteration {:d}".format(key_x, key_y, j))
 
-    exp.append([["observed", train_sc_obs, val_sc_obs, reg],
-                ["permuted", train_sc_rnd, val_sc_rnd, reg],
-                ["shifted", train_sc_shf, val_sc_shf, reg]])
+                x_norm = scaler.fit_transform(X=x[j, :, :])  # shape=(n_samples, n_features)
 
-save(exp, "/project/3011085.04/snn/data/results/learning-curve_LR-symbol-100-500-1000.pkl")
+                accuracy = cross_val_score(estimator=logit, X=x_norm, y=y, cv=5, scoring="balanced_accuracy")
+                scores.append(accuracy)
 
-plot_scores(exp[0], title="Validation curve (LR): symbol classification (100 neurons)",
-            xlabel="regularization parameter (C)", ylabel="balanced accuracy")
-
-plot_scores(exp[1], title="Validation curve (LR): symbol classification (500 neurons)",
-            xlabel="regularization parameter (C)", ylabel="balanced accuracy")
-
-plot_scores(exp[2], title="Validation curve (LR): symbol classification (1000 neurons)",
-            xlabel="regularization parameter (C)", ylabel="balanced accuracy")
-
+            save(scores, p.results + "/scores-1000-{}-{}.pkl".format(key_x, key_y))
